e71ba909c05ffccbdce8420389f4d2ec62b598f0
unknown <andrestiagos942@gmail.com>
2022-05-08 08:36:01 -0300

    v5

--- MaterialDelCurso/javascript_course/js/scripts.js
+++ MaterialDelCurso/javascript_course/js/scripts.js
@@ -6,3 +6,14 @@
 console.log("Statement 1");
 console.log("Statement 2");
 
+
+// V4 - trabajando con el DOM = Document Object Model
+
+/* Llamar al contenido del html */
+//document.getElementById("blue_box").innerHTML;
+/*  editar el contenido */
+//document.getElementById("blue_box").innerHTML = "lala";
+/*  editar el contenido + html interno */
+//document.getElementById("blue_box").innerHTML = "<h1>lala</h1>";
+/*  editar el contenido + html interno y mostrarlo por consola */
+console.log( document.getElementById("blue_box").innerHTML = "<h1>lala</h1>");
