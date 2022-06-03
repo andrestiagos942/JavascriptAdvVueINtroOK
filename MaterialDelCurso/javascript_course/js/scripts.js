582ca9ead5ee8438dc086ecd45b52af584c7b087
unknown <andrestiagos942@gmail.com>
2022-06-03 08:40:48 -0300

    v10

---
+++ MaterialDelCurso/javascript_course/js/scripts.js
@@ -0,0 +1,12 @@
+ 
+
+
+ var orderValue = 100;
+
+ if (orderValue >= 1000 ) {
+    console.log(" sos elegible para financiar tu orden ");
+ }
+ else
+ {
+    console.log("Opciones de financiamiento no son permitidas para menores a 1k verde bb");    
+ }
