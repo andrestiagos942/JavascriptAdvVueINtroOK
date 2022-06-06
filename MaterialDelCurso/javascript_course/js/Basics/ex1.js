6e59b102ef4b3104d14e8e50890c5ba8245b5667
unknown <andrestiagos942@gmail.com>
2022-06-06 08:44:30 -0300

    v12 ejercicios

---
+++ MaterialDelCurso/javascript_course/js/Basics/ex1.js
@@ -0,0 +1,18 @@
+
+// EJERCICIO 1
+console.log("This is ex1.js ");
+
+// EJERCICIO 2  
+var user_name = "Andi";
+var ACAVARIABLEEDAD = "28";
+document.getElementById("student_message").innerHTML = "Hi, my name is " + user_name + " , I'm " + ACAVARIABLEEDAD + " years old and I'm learning Javascript";
+
+// EJERCICIO 3
+
+
+var NUM_1 = document.getElementById("num_1").innerHTML;
+var NUM_2 = document.getElementById("num_2").innerHTML;
+var average = (NUM_1 + NUM_2) / 2;
+
+document.getElementById("result").innerHTML =  average;
+
